c821308765cea925d4e885d492cf3bd94c4d7b4e
Kevin Mas Ruiz <masruizkevin@gmail.com>
2016-10-20 21:58:43 +0200

    initial commit from initializr

---
+++ src/main/java/com/github/kmruiz/workshops/DddHeuristicsApplication.java
@@ -0,0 +1,12 @@
+package com.github.kmruiz.workshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddHeuristicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddHeuristicsApplication.class, args);
+	}
+}
